e42bc58fbfe97b24a72f3570ba7ad3b7a69f3de3
saegeullee <lemix777@naver.com>
2020-07-28 03:27:59 +0900

    Implemented todo item create request validation in api gateway

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -16,11 +16,6 @@ const secretManagerClient = new AWS.SecretsManager()
 
 let cachedSecret: string
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = 'https://dev-k68389gq.us.auth0.com/.well-known/jwks.json'
-
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
@@ -63,12 +58,6 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-
   const secretObject: any = await getSecret()
   const secret = secretObject[secretField]
 
